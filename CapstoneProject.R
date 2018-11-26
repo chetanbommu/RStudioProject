e7b789c19f0522ceeaa56bde7eba5c33ed4cdfad
sai <puram.saikurpa@gmail.com>
2018-11-26 20:21:34 +0530

    numeric to facotr conversion

--- CapstoneProject.R
+++ CapstoneProject.R
@@ -1,1 +1,31 @@
-##Testing git commit in project
+##Consider the target variable as CustServ.Calls
+getwd()
+churn_mv=read.csv("churn_MV.csv")
+
+names=colnames(churn_mv)
+
+charCols=c("Churn","Intl.Plan","VMail.Plan","State","Area.Code","Phone")
+numCols=setdiff(names,charCols)
+numCols
+
+
+## data tranformation
+
+colToFactos<-function(x){
+  x=as.factor(x)
+}
+churn_mv[,charCols]=lapply(churn_mv[,charCols], colToFactos)
+str(churn_mv)
+
+## null values imputation
+
+
+
+
+
+
+
+
+
+
+
