e2b7f0f55636582995ee0c7059e6f4e9e828cac9
sai <puram.saikurpa@gmail.com>
2018-12-04 16:31:40 +0530

    logistic regression practice file commited

--- LogisticRegPractice.R
+++ LogisticRegPractice.R
@@ -7,17 +7,21 @@ summary(universalBank)
 ## if we have a featue with binary data no need to convert them into categorical because if we convert internally it will convert to binary again
 ## ordinal feature is the one which have an order in it. if there is no order we will consider it as categorical variable
 
-
-universalBank$Family=as.factor(universalBank$Family)
-universalBank$Education=as.factor(universalBank$Education)
 universalBank$Personal.Loan=as.factor(universalBank$Personal.Loan)
-universalBank$Securities.Account=as.factor(universalBank$Securities.Account)
-universalBank$CD.Account=as.factor(universalBank$CD.Account)
-universalBank$Online=as.factor(universalBank$Online)
-universalBank$CreditCard=as.factor(universalBank$CreditCard)
 universalBank$ZIP.Code=as.factor(universalBank$ZIP.Code)
 
 
+## spliting
+rows=1:nrow(universalBank)
+randRows=sample(rows,round(nrow(universalBank)*0.8))
+traiData=universalBank[randRows,]
+testData=universalBank[-randRows,]
 
+
+## logistic regression model
+model=glm(Personal.Loan~. -ZIP.Code,data = universalBank,family = "binomial")
+summary(model)
+
+preds=predict(model,testData,type = "response")
 
 
